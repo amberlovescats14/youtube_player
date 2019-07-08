7f8ba6247fe2b6e23151f9666f3a8fa28a77f094
Amber Jones <amberlovescats14@gmail.com>
2019-07-08 10:44:32 -0500

    packages

---
+++ music_player/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import YouTube from './components/YouTube'
+
+function App() {
+  return (
+    <div className="App">
+      <YouTube/>
+    </div>
+  );
+}
+
+export default App;
---
+++ music_player/src/components/YouTube.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class YouTube extends Component {
+  render() {
+    return (
+      <div>
+        <h1>YouTube</h1>
+      </div>
+    )
+  }
+}
